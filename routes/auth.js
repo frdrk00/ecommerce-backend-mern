2537d00db71c83acec20d6e040cbc358e9fe656b
frdrk00 <masterkooo@gmail.com>
2023-09-12 21:53:31 +0200

    JWT

--- routes/auth.js
+++ routes/auth.js
@@ -39,9 +39,20 @@ router.post('/login', async (req, res) => {
     OriginalPassword !== req.body.password &&
       res.status(401).json('Wrong credentials!')
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      {
+        expiresIn: '3d',
+      }
+    )
+
     const { password, ...others } = user._doc
 
-    res.status(200).json(others)
+    res.status(200).json({ others, accessToken })
   } catch (err) {
     res.status(500).json(err)
   }
