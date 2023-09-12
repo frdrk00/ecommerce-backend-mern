89a17ea91bb3f1c4cab64fff40e2409ede4bc0cd
frdrk00 <masterkooo@gmail.com>
2023-09-12 20:47:26 +0200

    Auth

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+
 
 dotenv.config()
 
@@ -16,7 +18,7 @@ mongoose
   })
 
 app.use(express.json())
-
+app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 
 app.listen(process.env.PORT || 5000, () => {
